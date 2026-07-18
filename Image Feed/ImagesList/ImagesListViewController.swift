5dc5b59df7931179e9381460da5f13e3a3b39b4e
lulu0llli <35fdfbjhd@gmail.com>
2026-07-19 00:54:47 +0300

    Задача по 8 спринту, лента с фотографиями

---
+++ Image Feed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,79 @@
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.rowHeight = 200
+        
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    private let photosName: [String] = Array(0..<20).map { "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+
+        configCell(
+            for: imageListCell,
+            with: indexPath
+        )
+
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        let imageName = photosName[indexPath.row]
+        
+        guard let image = UIImage(named: imageName) else { return }
+            cell.cellImage.image = image
+        
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        if indexPath.row % 2 == 0 {
+            cell.likeButton.setImage(UIImage(named: "like_button_on"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
+        }
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 200
+        }
+    
+    let imageWidth = image.size.width
+    let imageHeight = image.size.height
+    let tableViewWidth = tableView.bounds.width
+    
+    let scale = tableViewWidth / imageWidth
+    let cellHeight = imageHeight * scale + 8
+    
+    return cellHeight}
+}
